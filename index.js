27f119ef28f4322f3513cc08da2fffb4ab407658
Clodeday <cloderaldo@gmail.com>
2019-08-22 20:00:21 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const {Client,Attachment} = require('discord.js');
+const bot = new Client();
+
+
+
+const token = process.env.token;
+const PREFIX = '!';
+
+bot.on('ready',() =>{
+    console.log('Ready');
+});
+
+bot.on('message',(msg) =>{
+    let args = msg.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'hi':
+            msg.reply('Hello');
+            break;
+    }
+});
+
+
+bot.login(token);
